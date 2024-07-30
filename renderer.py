b892f9cd02c549897cf11b0d52750e69e7f31232
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2024-07-30 14:01:55 +0200

    changed renderer

--- renderer.py
+++ renderer.py
@@ -1,17 +1,19 @@
 import os
-import markdown
+import mistune
+from mistune.plugins.table import table
 
 path = "docs/eng/"
 files = os.listdir(path)
 to_replace = "#MARKDOWN#"
 output_path = "dist/"
+markdown = mistune.create_markdown(plugins=[table, 'footnotes', 'strikethrough'])
 with open("render_template.dat") as template:
     template_contents = template.read()
     for file in files:
         output_file_path = output_path + file.split(".")[0] + ".html"
         with open(path + file) as f:
             md = f.read()
-            rendered = markdown.markdown(md)
+            rendered = markdown(md)
             generated_html = template_contents.replace(to_replace, rendered)
             with open(output_file_path, "w") as output_file:
                 output_file.write(generated_html)
